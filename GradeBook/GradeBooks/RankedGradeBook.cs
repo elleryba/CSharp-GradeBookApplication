b52aca6d9872de2f8721a579ad892cb1435382ca
Ryba <I821209@NGIC.COM>
2020-10-06 12:59:48 -0400

    Submitting progress

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,10 +1,21 @@
-﻿namespace GradeBook.GradeBooks
+﻿using System;
+
+namespace GradeBook.GradeBooks
 {
     public  class RankedGradeBook : BaseGradeBook
     {
         public RankedGradeBook(string name) : base(name)
         {
             Type = Enums.GradeBookType.Ranked;
+        }
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if()
+            {
+                throw InvalidOperationException;
+                return 'F';
+            }
+            return 'F';
         }
     }
 }
